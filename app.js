2aa589441fb32a21dcdb64431c53c7d3898d877b
Michał Buczek <michal@buczek.cc>
2016-11-02 17:18:39 +0100

    postprocesor chaning unit, stoke nd fill color of path

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const cli = require('cli');
+
+const Postprocersor = require('./lib/Postprocersor');
+
+
+cli.parse({
+    unit: ['u', 'Set unit to', 'string'],
+    pathStroke: ['s', 'Set path stroke color', 'string'],
+    pathStrokeWidth: ['w', 'Set path stroke width', 'decimal'],
+    pathFill: ['f', 'Set path fill color', 'string']
+});
+
+cli.main((args, options) => {
+    let file = args[0];
+
+    if ( ! file) {
+        return console.error('Specify input file');
+    }
+
+    let pp = new Postprocersor(options);
+    pp.process(file, (err, result) => {
+        if (err) {
+            return console.error("Error:", err);
+        }
+
+        console.log(result.parse.html());
+    });
+
+});
